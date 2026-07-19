05fb48b117aa44d366379509ff0854548c5fde2c
Steve Hardister <shardister@gmail.com>
2026-07-19 10:54:03 -0400

    Created UE5 Asteroid Defense Project

---
+++ Source/AsteroidDefense56/VisualStudioConversion.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VisualStudioConversion.h"
+
+VisualStudioConversion::VisualStudioConversion()
+{
+}
+
+VisualStudioConversion::~VisualStudioConversion()
+{
+}
